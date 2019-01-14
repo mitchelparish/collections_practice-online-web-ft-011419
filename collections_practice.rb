cf0cfbadcde57902a176fb7e057cbd5138396bba
Mitchel Parish <mitchelparish@gmail.com>
2019-01-14 19:28:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,10 @@ def sort_array_asc(array_of_integers)
   array_of_integers.sort do |a, b|
     a <=> b
   end
-
 end
 
+def sort_array_desc(array_of_integers)
+  array_of_integers.sort do |a, b|
+    b <=> a
+  end
+end
